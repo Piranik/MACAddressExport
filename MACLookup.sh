d5cefd5e758d26359f145ccb0a731cc662be5cfe
Walker Sorensen <wsoren1@mc2school.org>
2018-04-13 13:41:32 -0400

    code can read base xml file and output info, next is to create new objects based on arp tables

--- MACLookup.sh
+++ MACLookup.sh
@@ -3,5 +3,4 @@
 str=$(arp -a) 
 
 str=$(echo ${str} | grep en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
-echo ${str}
 python compareMACSerial.py ${str[@]}
--- compareMACSerial.py
+++ compareMACSerial.py
@@ -4,22 +4,24 @@ import xml.etree.ElementTree as ET
 def main():
 
     mac_addresses = sys.argv
-    i = 0
-    for address in mac_addresses:
-    	print(address + " Device" + str(i))
-    	i += 1
 
 
 
 
-tree = ET.parse('studentObjects.xml')
+tree = ET.parse('/Users/walkersorensen/Dropbox/MACAddressExport/studentObjects.xml')
 root = tree.getroot()
-
-
+i = 0
+for child in root:
+	i += 1
+	print
+	print("--------------------[Device Number " + str(i) + "]--------------------")
+	print("MAC Address---------:", child[0].text)
+	print("Serial Number-------:", child[1].text)
+	print("Date last Checked in:", child[2].text)
+	print("---------------------------------------------------------")
 
 if __name__ == '__main__':
   main()
 
 
 
-print(root)
