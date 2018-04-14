a90d9a705921eb0e0209b0cde967ed0c46f817d7
walker <wsorensenm1@gmail.com>
2018-04-13 20:47:35 -0400

    branch init

--- compareMACSerial.py
+++ compareMACSerial.py
@@ -2,26 +2,32 @@ import sys
 import xml.etree.ElementTree as ET
 
 def main():
-
     mac_addresses = sys.argv
+    for address in mac_addresses:
+    	print(address)
+
+def generateReport(): #Prints report (NEED TO CONVERT TO RETURN MULTILINE STR) 
+	i = 0
+	for device in root:
+		if device[3].text == 'true':
+			i += 1
+			print
+			print("-----------------[Device Number " + str(i) + "]----------------")
+			print("MAC Address---------: " + device[0].text)
+			print("Serial Number-------: " + device[1].text)
+			print("Date last Checked in: " + device[2].text)
+			print("--------------------------------------------------")
+			print
 
 
 
-
-tree = ET.parse('/Users/walkersorensen/Dropbox/MACAddressExport/studentObjects.xml')
+tree = ET.parse('studentObjects.xml')
 root = tree.getroot()
-i = 0
-for child in root:
-	i += 1
-	print
-	print("--------------------[Device Number " + str(i) + "]--------------------")
-	print("MAC Address---------:", child[0].text)
-	print("Serial Number-------:", child[1].text)
-	print("Date last Checked in:", child[2].text)
-	print("---------------------------------------------------------")
+
 
 if __name__ == '__main__':
   main()
 
+generateReport()
 
-
+print("compareMACSerial.py complete")
